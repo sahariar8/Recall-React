c23e0468299b00b559ead4efdccda783b38c9d82
sahariar8 <sahariaralam8@gmail.com>
2025-01-15 12:12:53 +0600

    feat:RR(Rcall REACT)

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <>
+      <h1 className="text-xl text-center text-green-500 bg-slate-800 p-2">We are here to learn React from tapaScript</h1>
+    </>
+  )
+}
+
+export default Header
